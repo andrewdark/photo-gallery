ed013d831eb7ace393a169397417c14929e1f196
andrewdark <samsonov.a@ukr.net>
2023-08-31 13:48:46 +0300

    add favicon

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+import myIcon from '../images/icons8-website-64.png';
+
+var link = document.querySelector("link[rel~='icon']");
+if (!link) {
+  link = document.createElement('link');
+  link.rel = 'icon';
+  document.head.appendChild(link);
+}
+link.href = myIcon;
