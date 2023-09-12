47c3b26adb70dc1180501f5d998866e0814fb51f
andrewdark <samsonov.a@ukr.net>
2023-09-12 07:44:08 +0000

    install and import dependencies notiflix, simplelightbox

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,7 @@
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { fetchImages } from './photo-api.js';
 import myIcon from '../images/icons8-website-64.png';
 
 var link = document.querySelector("link[rel~='icon']");
